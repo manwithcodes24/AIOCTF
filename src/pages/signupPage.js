34b617fa466d11951d33ff2e09bb6e4d555449db
yash <thisform24@gmail.com>
2020-06-18 17:16:30 +0530

    update

--- src/pages/signupPage.js
+++ src/pages/signupPage.js
@@ -246,7 +246,7 @@ fetch('http://localhost:8000/user/UserRegister', {
           <div className="overlay-panel overlay-left">
             <h1>Hello, Friend!</h1>
             <p>Enter your details and start journey with us</p>
-            <button> className="ghost" id="signUp">Sign Up</button>
+            <button className="ghost" id="signUp">Sign Up</button>
             {status=='200' ?
              ( <Alert severity="success">
               This is a success alert — check it out!</Alert> ) : (<div> </div> )}
